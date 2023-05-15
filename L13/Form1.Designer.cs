568fa29ec7b2f110e55a02ecc288f67e20b2c819
Liubov Storozhenko <lybov5656@gmail.com>
2023-05-15 21:11:24 +0300

    Добавьте файлы проекта.

---
+++ L13/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace L13
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fileSystemTreeView = new System.Windows.Forms.TreeView();
+            this.labelFiles = new System.Windows.Forms.Label();
+            this.filePropertiesLabel = new System.Windows.Forms.Label();
+            this.filterTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fileSystemTreeView
+            // 
+            this.fileSystemTreeView.Location = new System.Drawing.Point(386, 36);
+            this.fileSystemTreeView.Name = "fileSystemTreeView";
+            this.fileSystemTreeView.Size = new System.Drawing.Size(865, 271);
+            this.fileSystemTreeView.TabIndex = 2;
+            this.fileSystemTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.fileSystemTreeView_AfterSelect);
+            // 
+            // labelFiles
+            // 
+            this.labelFiles.AutoSize = true;
+            this.labelFiles.BackColor = System.Drawing.Color.GhostWhite;
+            this.labelFiles.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.labelFiles.Location = new System.Drawing.Point(637, 13);
+            this.labelFiles.Name = "labelFiles";
+            this.labelFiles.Size = new System.Drawing.Size(310, 20);
+            this.labelFiles.TabIndex = 6;
+            this.labelFiles.Text = "Відображення структури папок і файлів";
+            // 
+            // filePropertiesLabel
+            // 
+            this.filePropertiesLabel.AutoSize = true;
+            this.filePropertiesLabel.BackColor = System.Drawing.Color.White;
+            this.filePropertiesLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.filePropertiesLabel.Location = new System.Drawing.Point(35, 36);
+            this.filePropertiesLabel.Name = "filePropertiesLabel";
+            this.filePropertiesLabel.Size = new System.Drawing.Size(70, 20);
+            this.filePropertiesLabel.TabIndex = 8;
+            this.filePropertiesLabel.Text = "Довідка";
+            this.filePropertiesLabel.Click += new System.EventHandler(this.Form1_Load);
+            // 
+            // filterTextBox
+            // 
+            this.filterTextBox.Location = new System.Drawing.Point(12, 263);
+            this.filterTextBox.Multiline = true;
+            this.filterTextBox.Name = "filterTextBox";
+            this.filterTextBox.Size = new System.Drawing.Size(339, 44);
+            this.filterTextBox.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
+            this.label1.Location = new System.Drawing.Point(89, 243);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(187, 17);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Фільтр каталогів та файлів";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(716, 357);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(456, 297);
+            this.pictureBox1.TabIndex = 13;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(27, 357);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(657, 297);
+            this.textBox1.TabIndex = 14;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label2.Location = new System.Drawing.Point(230, 332);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 18);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "Перегляд текстових файлів";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label3.Location = new System.Drawing.Point(846, 332);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(207, 18);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Перегляд графічних об\'єктів";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(192)))));
+            this.ClientSize = new System.Drawing.Size(1291, 680);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.filterTextBox);
+            this.Controls.Add(this.filePropertiesLabel);
+            this.Controls.Add(this.labelFiles);
+            this.Controls.Add(this.fileSystemTreeView);
+            this.Name = "Form1";
+            this.Text = "L13";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TreeView fileSystemTreeView;
+        private System.Windows.Forms.Label labelFiles;
+        private System.Windows.Forms.Label filePropertiesLabel;
+        private System.Windows.Forms.TextBox filterTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ L13/Form1.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Diagnostics;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace L13
+{
+    public partial class Form1 : Form
+    {
+        private DirectoryInfo directoryInfo;
+        public Form1()
+        {
+            InitializeComponent();
+            this.fileSystemTreeView.BeforeExpand += new System.Windows.Forms.TreeViewCancelEventHandler(this.fileSystemTreeView_BeforeExpand);
+            this.fileSystemTreeView.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.fileSystemTreeView_AfterSelect);
+            this.filterTextBox.TextChanged += new System.EventHandler(this.filterTextBox_TextChanged);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadDrives();
+        }
+        private void LoadDrives()
+        {
+            DriveInfo[] drives = DriveInfo.GetDrives();
+
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                driveNode.Tag = drive;
+
+                if (drive.IsReady == true)
+                {
+                    try
+                    {
+                        foreach (DirectoryInfo dir in drive.RootDirectory.GetDirectories())
+                        {
+                            TreeNode dirNode = new TreeNode(dir.Name);
+                            dirNode.Tag = dir;
+                            dirNode.Nodes.Add("");
+
+                            driveNode.Nodes.Add(dirNode);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                    }
+                }
+
+                fileSystemTreeView.Nodes.Add(driveNode);
+            }
+        }
+        private void fileSystemTreeView_BeforeExpand(object sender, TreeViewCancelEventArgs e)
+        {
+            TreeNode currentNode = e.Node;
+
+            if (currentNode.Nodes.Count == 1 && currentNode.Nodes[0].Text == "")
+            {
+                currentNode.Nodes.Clear();
+                directoryInfo = (DirectoryInfo)currentNode.Tag;
+                try
+                {
+                    foreach (DirectoryInfo subDir in directoryInfo.GetDirectories())
+                    {
+                        TreeNode node = new TreeNode(subDir.Name);
+                        node.Tag = subDir;
+                        node.Nodes.Add("");
+                        currentNode.Nodes.Add(node);
+                    }
+                    foreach (FileInfo file in directoryInfo.GetFiles())
+                    {
+                        TreeNode node = new TreeNode(file.Name);
+                        node.Tag = file;
+                        currentNode.Nodes.Add(node);
+                    }
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+                    MessageBox.Show($"Access denied: {ex.Message}");
+                }
+            }
+        }
+        private void fileSystemTreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            if (e.Node.Tag is FileInfo)
+            {
+                FileInfo file = (FileInfo)e.Node.Tag;
+
+                if (IsTextFile(file))
+                {
+                    try
+                    {
+                        string fileContent;
+                        using (StreamReader reader = new StreamReader(file.FullName, Encoding.UTF8))
+                        {
+                            fileContent = reader.ReadToEnd();
+                        }
+                        textBox1.Text = fileContent;
+                        filePropertiesLabel.Text = $"Name: {file.Name}\nSize: {file.Length} bytes\nCreation time: {file.CreationTime}\nAttributes: {file.Attributes.ToString()}";
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error reading file: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else if (IsImageFile(file))
+                {
+                    try
+                    {
+                        Image image = Image.FromFile(file.FullName);
+                        Image thumbnail = image.GetThumbnailImage(pictureBox1.Width, pictureBox1.Height, null, IntPtr.Zero);
+                        pictureBox1.Image = thumbnail;
+
+                        filePropertiesLabel.Text = $"Name: {file.Name}\nSize: {file.Length} bytes\nCreation time: {file.CreationTime}\nAttributes: {file.Attributes.ToString()}\n\nImage dimensions: {thumbnail.Width} x {thumbnail.Height}";
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show($"Error loading image: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                else
+                {
+                    filePropertiesLabel.Text = $"Name: {file.Name}\nSize: {file.Length} bytes\nCreation time: {file.CreationTime}\nAttributes: {file.Attributes.ToString()}\n\nCannot display content for this file type.";
+                }
+            }
+            else if (e.Node.Tag is DirectoryInfo)
+            {
+                DirectoryInfo dir = (DirectoryInfo)e.Node.Tag;
+                filePropertiesLabel.Text = $"Name: {dir.Name}\nCreation time: {dir.CreationTime}\nAttributes: {dir.Attributes.ToString()}";
+            }
+            else if (e.Node.Tag is DriveInfo)
+            {
+                DriveInfo drive = (DriveInfo)e.Node.Tag;
+                filePropertiesLabel.Text = $"Drive letter: {drive.Name}\nDrive type: {drive.DriveType}\nAvailable space: {drive.AvailableFreeSpace} bytes";
+            }
+        }
+
+        private bool IsTextFile(FileInfo file)
+        {
+            string extension = file.Extension.ToLower();
+            return extension == ".txt" || extension == ".csv" || extension == ".log" || extension == ".doc" || extension == ".docx" || extension == ".zip" || extension == ".pdf";
+        }
+
+        private bool IsImageFile(FileInfo file)
+        {
+            string extension = file.Extension.ToLower();
+            return extension == ".jpg" || extension == ".jpeg" || extension == ".png" || extension == ".gif";
+        }
+
+        private void filterTextBox_TextChanged(object sender, EventArgs e)
+        {
+            string filterText = filterTextBox.Text.ToLower();
+            ApplyFilter(filterText);
+        }
+        private void clearFilterButton_Click(object sender, EventArgs e)
+        {
+            filterTextBox.Text = "";
+            ResetTreeView();
+        }
+
+        private void filterFilesButton_Click(object sender, EventArgs e)
+        {
+            ApplyFilter("*.txt");
+        }
+
+        private void filterDirectoriesButton_Click(object sender, EventArgs e)
+        {
+            ApplyFilter("DIR");
+        }
+
+        private void ApplyFilter(string filterText)
+        {
+            fileSystemTreeView.BeginUpdate();
+            fileSystemTreeView.Nodes.Clear();
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            foreach (DriveInfo drive in drives)
+            {
+                TreeNode driveNode = new TreeNode(drive.Name);
+                driveNode.Tag = drive;
+                if (drive.IsReady == true)
+                {
+                    try
+                    {
+                        foreach (DirectoryInfo dir in drive.RootDirectory.EnumerateDirectories("*", SearchOption.TopDirectoryOnly)
+                                                                         .Where(d => d.Name.ToLowerInvariant().Contains(filterText)))
+                        {
+                            TreeNode dirNode = new TreeNode(dir.Name);
+                            dirNode.Tag = dir;
+                            dirNode.Nodes.Add("");
+
+                            driveNode.Nodes.Add(dirNode);
+                        }
+                        foreach (FileInfo file in drive.RootDirectory.EnumerateFiles("*", SearchOption.TopDirectoryOnly)
+                                                                     .Where(f => f.Name.ToLowerInvariant().Contains(filterText)))
+                        {
+                            TreeNode fileNode = new TreeNode(file.Name);
+                            fileNode.Tag = file;
+
+                            driveNode.Nodes.Add(fileNode);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                    }
+                }
+                fileSystemTreeView.Nodes.Add(driveNode);
+            }
+            fileSystemTreeView.EndUpdate();
+        }
+        private void ResetTreeView()
+        {
+            fileSystemTreeView.BeginUpdate();
+            fileSystemTreeView.Nodes.Clear();
+            LoadDrives();
+            fileSystemTreeView.EndUpdate();
+        }
+    }
+}
